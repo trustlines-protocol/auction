4c9eaf3a2f7c9ac9b49a700be89de5dc76e9bbcb
Maximilian Schmidt <max@eth.events>
2019-04-17 13:51:40 +0200

    add more randomness to the testdata to show more realistic bids

--- validator-auction-rest-server/lib/NoEthEventsClient.js
+++ validator-auction-rest-server/lib/NoEthEventsClient.js
@@ -20,10 +20,11 @@ export default class NoEthEventsClient extends EthEventsClient {
         const auctionStart = this.getAuctionStartInSeconds()
         const result = []
         for (let i = 1; i < 100; ++i) {
-            const ts = auctionStart + (i * NoEthEventsClient.randomInt(100, 1200))
+            const ts = auctionStart + (i * NoEthEventsClient.randomInt(500, 5200))
+            const v = EthEventsClient.getCurrentPrice(auctionStart * 1000, ts * 1000) * NoEthEventsClient.randomFloat(1,3,2)
             result.push({
                 bidder: randomHex(20),
-                bidValue: EthEventsClient.getCurrentPrice(auctionStart * 1000, ts * 1000),
+                bidValue: v,
                 timestamp: ts
             })
         }
@@ -32,10 +33,17 @@ export default class NoEthEventsClient extends EthEventsClient {
 
     getWhitelistedAddresses() {
         // TODO
-        return []
+        return ['0xAA']
     }
 
     static randomInt(low, high) {
         return Math.floor(Math.random() * (high - low) + low)
     }
+
+    static randomFloat(min, max, decimalPlaces) {
+        const rand = Math.random() * (max - min) + min
+        const power = Math.pow(10, decimalPlaces)
+        return Math.floor(rand * power) / power
+    }
+
 }
